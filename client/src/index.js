0a8c4fc14755fd7ffc45d1361933047e3161d502
aanogueira99 <61246436+aanogueira99@users.noreply.github.com>
2021-09-29 15:10:19 +0100

    Primeira iteração da App

--- client/src/index.js
+++ client/src/index.js
@@ -5,13 +5,24 @@ import App from "./App";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Auth0ProviderWithHistory from "./auth/auth0-provider-with-history";
 import { BrowserRouter as Router} from "react-router-dom";
+import reportWebVitals from './reportWebVitals';
 
-
-ReactDOM.render(
-  <Router>
+let startApp = () => {
+  ReactDOM.render(
+    <Router>
     <Auth0ProviderWithHistory>
       <App />
     </Auth0ProviderWithHistory>
   </Router>,
   document.getElementById("root")
-);
+  );
+}
+
+if(!window.cordova) {
+  startApp()
+} else {
+  document.addEventListener('deviceready', startApp, false)
+}
+
+reportWebVitals();
+
