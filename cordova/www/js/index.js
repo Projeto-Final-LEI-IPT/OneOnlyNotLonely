b0e2ab840b2872036963ffd99a1a7c485c089d44
aanogueira99 <61246436+aanogueira99@users.noreply.github.com>
2021-10-03 00:53:36 +0100

    app 2

--- cordova/www/js/index.js
+++ cordova/www/js/index.js
@@ -1,24 +1,73 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+var container = document.getElementById('container');
 
-// Wait for the deviceready event before using any of Cordova's device APIs.
-// See https://cordova.apache.org/docs/en/latest/cordova/events/events.html#deviceready
+var app={
+    // Application Constructor
+    initialize: function () {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+
+    },
+    onDeviceReady: function () {
+        this.receivedEvent('deviceready');
+    
+        var onSuccess = function(position){
+            var topo = document.getElementById("topo");
+            var body = document.body;
+            var mapa = document.getElementById("mapid");
+            var btn = document.getElementById("btMapa")
+
+            var mymap = L.map('mapid').setView([39.60360511, -8.40795278], 16);
+
+            function onOnline() {
+                L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+                attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+                maxZoom: 18,
+                tileSize: 512,
+                zoomOffset: -1
+            }).addTo(mapa);
+            }
+                onOnline();
+
+            //vai buscar a posição inicial quando inicia a app
+            navigator.geolocation.getCurrentPosition(function (location) {
+                var latlng = new L.LatLng(location.coords.latitude, location.coords.longitude);
+            });
+            mymap.on('locationfound', onLocationFound);
+
+            
+
+            mapa.locate({setView: true, maxZoom: 16});
+
+            btn.onclick = mostraMapa =>{
+                topo.classList.add('hidden');
+                btn.classList.add('hidden');
+                mapa.classList.remove('hidden');
+            }
+            
+        }
+        var onError = function (error) {
+            str = 'code: ' + error.code + '\n' +
+            'message: ' + error.message + '\n';
+            if (error.code == 3) { // no gps
+                str = "Sem sinal de GPS.";
+                onSuccess();
+            } 
+            alert(str);
+        }
+
+        navigator.geolocation.getCurrentPosition(onSuccess, onError, { timeout: 10000, enableHighAccuracy: true });
+    },
+    
+    // Update DOM on a Received Event
+    receivedEvent: function (id) {
+        var parentElement = document.getElementById(id);
+    }
+}
+app.initialize();
+
+/*
+
+export default withAuthenticationRequired(Mapa)
+
 document.addEventListener('deviceready', onDeviceReady, false);
 
 function onDeviceReady() {
@@ -26,4 +75,4 @@ function onDeviceReady() {
 
     console.log('Running cordova-' + cordova.platformId + '@' + cordova.version);
     document.getElementById('deviceready').classList.add('ready');
-}
+}*/
